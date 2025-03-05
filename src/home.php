41b827cea28b423c4cefbf159c766d1abfee05a5
Anlaharpanda2 <Anlaharpanda2@gmail.com>
2025-03-05 10:36:23 +0700

    edit_index.php_dan_home.php

--- src/home.php
+++ src/home.php
@@ -1,7 +1,46 @@
-<h1>Hello, world!</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Numquam omnis corporis tempora quae, ad exercitationem id ex maxime asperiores ea sit maiores,
-non aliquam doloremque sed est voluptas quo, dignissimos magnam quia quis officiis. Numquam,
-necessitatibus totam ut perspiciatis quo nostrum libero ipsa reiciendis repudiandae labore est quas architecto. Id.</p>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis doloremque, vel voluptate suscipit magni recusandae.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repellendus, voluptate ipsum dignissimos necessitatibus qui, in hic possimus repudiandae mollitia aspernatur facilis labore. Nisi adipisci animi aperiam totam, ullam necessitatibus.</p>
+<?php
+include 'koneksi.php';
+
+// Menghitung jumlah data
+$dosen = mysqli_fetch_array(mysqli_query($db, "SELECT COUNT(*) as total FROM dosen"))['total'];
+$mahasiswa = mysqli_fetch_array(mysqli_query($db, "SELECT COUNT(*) as total FROM mahasiswa"))['total'];
+$prodi = mysqli_fetch_array(mysqli_query($db, "SELECT COUNT(*) as total FROM prodi"))['total'];
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistem Informasi Akademik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="row gx-3 gy-2 align-items-center py-5">
+            <div class = "col-lg-4 px-5">
+                <div class="bg-secondary p-4 d-flex align-items-center justify-content-between rounded-2">
+                    <img src="mahasiswa.png" class="img-fluid rounded-2" width="80">
+                    <p class="text-white m-0 fs-4">mahasiswa <br><?php echo $mahasiswa; ?> Data</p>
+                </div>
+            </div>
+            <div class = "col-lg-4 px-5">
+                <div class="bg-secondary p-4 d-flex align-items-center justify-content-between rounded-2">
+                    <img src="prodi.png" alt="Dosen" class="img-fluid rounded-2" width="80">
+                    <p class="text-white m-0 fs-4">prodi <br><?php echo $prodi; ?> Data</p>
+                </div>
+            </div>
+            <div class = "col-lg-4 px-5">
+                <div class="bg-secondary p-4 d-flex align-items-center justify-content-between rounded-2">
+                    <img src="dosen.png" alt="Dosen" class="img-fluid rounded-2" width="80">
+                    <p class="text-white m-0 fs-4">dosen <br><?php echo $dosen; ?> Data</p>
+                </div>
+            </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptatibus quibusdam voluptates aspernatur fugit perferendis nisi sit. Sapiente at minus eum totam, perspiciatis, reprehenderit odit sunt aliquid ducimus nobis atque ipsum quod necessitatibus culpa? Amet tempora rem aliquid. Nisi praesentium dolorum, atque dignissimos ducimus vel vero sequi perferendis odit maiores cupiditate esse, voluptas animi magnam quos molestiae saepe odio! Doloribus dolore id quae vitae eligendi, optio laboriosam dicta repellendus sed, possimus exercitationem! Esse perferendis minima rerum magni tenetur molestias optio odit, ducimus temporibus inventore reiciendis ad modi. Totam iste commodi illo eligendi dolorem, quod repellat exercitationem sit excepturi aperiam. Quis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore at soluta aspernatur eos totam dolorum recusandae dolore nemo unde rerum. Error eaque fuga, corporis perspiciatis natus eveniet provident. Quod, rerum quasi. Repudiandae doloribus saepe assumenda itaque voluptatibus temporibus facere dolore architecto quod adipisci animi dolor neque, aut, vero, cumque possimus?</p>
+    </div>
+</body>
+</html>
--- src/index.php
+++ src/index.php
@@ -1,15 +1,22 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistem Informasi Akademik</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
-  </head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sistem Informasi Akademik</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    </head>
+        <style>
+        .custom-link:hover {
+            color: white !important; /* Warna berubah jadi putih saat hover */
+            text-decoration: none !important; /* Tetap tanpa garis bawah */
+        }
+    </style>
  
   <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar bg-dark navbar-expand-lg border-bottom border-body" data-bs-theme="dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Kelompok3</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +25,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                <a class="nav-link" aria-current="page" href="index.php">Home</a>
                 </li>
 
                 <li class="nav-item">
@@ -49,9 +56,41 @@
             if($page == "dosen") include 'dosen.php';
         ?>
     </div>
-    <div class="text-center bg-dark text-light py-2">
-        Sistem Informasi Akademik &copy; TRPL 2D Teknologi Informasi 2024
+    <footer class = "bg-dark">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class = "col-lg-3 py-5">
+                <img src="TI.png" class="img-fluid rounded-2" width="80">
+                <p class = "text-white py-2">Jurusan teknologi informasi</p>
+            </div>
+            <div class = "col-lg-3 py-5 text-white">
+                <p>
+                    <ul>
+                        <li>
+                            <a href=# class="custom-link text-secondary text-decoration-none">Lorem ipsum dolor sit</a>
+                        </li>
+                        <li>
+                            <a href=# class="custom-link text-secondary text-decoration-none">Lorem ipsum dolor sit amet</a>
+                        </li>
+                        <li>
+                            <a href=# class="custom-link text-secondary text-decoration-none">Lorem ipsum dolor sit</a>
+                        </li>
+                        <li>
+                            <a href=# class="custom-link text-secondary text-decoration-none">Lorem ipsum dolor sit amet</a>
+                        </li>
+                    </ul>
+                </p>
+            </div>
+            <div class = "col-lg-3 py-5">
+            </div>
+            <div class = "col-lg-3 py-5">
+                <i class="bi bi-instagram text-white"></i>
+                <i class="bi bi-facebook text-white"></i>
+                <i class="bi bi-tiktok text-white"></i>
+            </div>
+        </div>
     </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
